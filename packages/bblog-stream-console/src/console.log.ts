8b0c8c4b08eb94f7f120a1a47674d2c44c2efb17
Blayne Chard <blayne@chard.nz>
2017-03-14 19:40:09 +1300

    Create bblog-stream-console

---
+++ packages/bblog-stream-console/src/console.log.ts
@@ -0,0 +1,18 @@
+import { LogMessage, LogStream } from 'bblog';
+
+export class ConsoleLogStream implements LogStream {
+
+    constructor(private level: number) {
+    }
+
+    setLevel(level: number) {
+        this.level = level;
+    }
+
+    write(message: LogMessage) {
+        if (message.level < this.level) {
+            return;
+        }
+        console.log(JSON.stringify(message));
+    }
+}
